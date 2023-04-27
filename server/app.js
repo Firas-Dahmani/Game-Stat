62cb517a55920497cddb1d99b5999392e320d742
Firas Dahmani <firas1dahmani@gmail.com>
2023-04-27 15:04:55 +0200

    deploy dir

--- server/app.js
+++ server/app.js
@@ -14,19 +14,11 @@ app.use(express.json());
 app.use(express.json());
 app.use('/api', gameRoutes);
 
-const __dirname1 = path.resolve();
+app.use(express.static(path.join(__dirname, '..', 'server', 'public')));
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname1, "../client/dist")));
-
-  app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname1, "..", "client", "dist", "index.html"))
-  );
-} else {
-  app.get("/", (req, res) => {
-    res.send("API is running..");
-  });
-}
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'server', 'public', 'index.html'));
+});
 
 const PORT = process.env.PORT || 8000;
 module.exports = app.listen(PORT, () => {
