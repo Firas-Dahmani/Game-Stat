72b42e98426ce1f8a99a56386acfc82dab0daa0d
Firas Dahmani <firas1dahmani@gmail.com>
2023-04-27 14:46:12 +0200

    deployment

--- server/app.js
+++ server/app.js
@@ -2,6 +2,8 @@ require('dotenv').config()
 const express = require('express');
 const gameRoutes = require('./routes/gamesRoutes.js');
 const cors = require('cors')
+const path = require("path");
+
 const app = express()
 
 
@@ -11,6 +13,20 @@ app.use(express.json());
 
 app.use(express.json());
 app.use('/api', gameRoutes);
+
+const __dirname1 = path.resolve();
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname1, "/client/dist")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname1, "client", "dist", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running..");
+  });
+}
 
 const PORT = process.env.PORT || 8000;
 module.exports = app.listen(PORT, () => {
